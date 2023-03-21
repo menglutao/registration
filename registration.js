1dd04b0e09cfea5e4f66613ea32e1af105295c5e
menglutao <azraeltml@gmail.com>
2023-03-21 15:58:34 +0200

    Refactor code in js file and add emoji in css file

--- registration.js
+++ registration.js
@@ -1,76 +1,156 @@
-$(document).ready(function() {
-    $('#email').blur(function() {
-        var email = $('#email').val();
-        if (IsEmail(email) == false) {
-            $('#sign-up').attr('disabled', true);
-        } else {
-            $('#sign-up').attr('disabled', false);
-        }
-    });
-    $('#password').keyup(function() {
-        var password = $('#password').val();
-        if (checkStrength(password) == false) {
-            $('#sign-up').attr('disabled', true);
-        }
-      else {
-            $('#sign-up').attr('disabled', false);
-        }
-    });
+// remove jquery from this funtion check strength
+// refactor the code of checkStrength function to make it into a pure function
+//Add some basic test cases
 
 function IsEmail(email) {
-        var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
- /*In the regular expression ([a-zA-Z0-9]{2,4})+, the character set [a-zA-Z0-9] must appear between 2 and 4 times consecutively*/
-       return regex.test(email);
+  var regex =
+    /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  /*In the regular expression ([a-zA-Z0-9]{2,4})+, the character set [a-zA-Z0-9] must appear between 2 and 4 times consecutively*/
+  return regex.test(email);
+}
+
+// function checkStrength(password) {
+//   var strength = 0;
+//   // if 8+ characters
+
+//   if (password.length > 7) {
+//     strength += 1;
+//     $("#length").addClass("valid");
+//   } else {
+//     $("#length").removeClass("valid");
+//   }
+
+//   //If password contains  lower characters, increase strength value.
+//   if (password.match(/[a-z]+/)) {
+//     strength += 1;
+//     $("#lowercase").addClass("valid");
+//   } else {
+//     $("#lowercase").removeClass("valid");
+//   }
+
+//   //If password contains upper characters, increase strength value.
+//   if (password.match(/[A-Z]+/)) {
+//     strength += 1;
+//     $("#uppercase").addClass("valid");
+//   } else {
+//     $("#uppercase").removeClass("valid");
+//   }
+
+//   //If it has numbers increase strength value.
+//   if (password.match(/([0-9])/)) {
+//     strength += 1;
+//     $("#decimal").addClass("valid");
+//   } else {
+//     $("#decimal").removeClass("valid");
+//   }
+
+//   //If it has one special character, increase strength value.
+//   if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+//     strength += 1;
+//     $("#special").addClass("valid");
+//   } else {
+//     $("#special").removeClass("valid");
+//   }
+//   console.log(strength);
+
+//   if (strength > 4) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// Check password if contains certain characters or not and return the result
+function checkPassword(password){
+    let strength = 0;
+    const length = password.length;
+    const hasLowerCase = /[a-z]+/.test(password);
+    const hasUpperCase = /[A-Z]+/.test(password);
+    const hasNumber = /([0-9])+/.test(password);
+    const hasSpecialChar = /([!,%,&,@,#,$,^,*,?,_,~])+/.test(password)
+    // console.log(length, hasLowerCase, hasUpperCase, hasNumber, hasSpecialChar)
+    const passwordResult = {length, hasLowerCase, hasUpperCase, hasNumber, hasSpecialChar}
+    return passwordResult;
+}
+
+// Calculate the strength of the password
+function calculateStrength(passwordResult){
+
+    let strength = 0;
+    if(passwordResult.length > 7){
+        strength += 1;
     }
-  
-function checkStrength(password) {
-        var strength = 0;
-        // if 8+ characters
-        
-  if (password.length > 7) {
-            strength += 1;
-            $('#length').addClass('valid');
-        } else {
-            $('#length').removeClass('valid');           
-        }
-
-        //If password contains  lower characters, increase strength value.
-        if (password.match(/[a-z]+/)) {
-            strength += 1;
-           $('#lowercase').addClass('valid');
-
-
-        } else {
-            $('#lowercase').removeClass('valid');
-        }
-  
-   //If password contains upper characters, increase strength value.
-   if (password.match(/[A-Z]+/)) {
-            strength += 1;
-      $('#uppercase').addClass('valid');
+    if(passwordResult.hasLowerCase){
+        strength += 1;
     }
-     else{
-       $('#uppercase').removeClass('valid');
-       
-     }
-  
-        //If it has numbers increase strength value.
-        if (password.match(/([0-9])/)) {
-            strength += 1;
-           $('#decimal').addClass('valid');
+    if(passwordResult.hasUpperCase){
+        strength += 1;
+    }
+    if(passwordResult.hasNumber){
+        strength += 1;
+    }
+    if(passwordResult.hasSpecialChar){
+        strength += 1;
+    }
+    return strength;
+}
 
-        } else {
-            $('#decimal').removeClass('valid');
-        }
+// Check the strength of the password
+function checkStrength(passwordResult){
+    const strength = calculateStrength(passwordResult);
+    return strength > 4;
+}
 
-        //If it has one special character, increase strength value.
-        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
-              strength += 1;
-           $('#special').addClass('valid');
+// Update the UI of the password strength
+function updatePasswordStrengthUI(passwordResult){
 
-        } else {
-            $('#special').removeClass('valid');
-        }
-   }
-    
+    if(passwordResult.length > 7){
+        $("#length").addClass("valid");
+    }else{
+        $("#length").removeClass("valid");
+    }
+    if(passwordResult.hasLowerCase){
+        $("#lowercase").addClass("valid");
+    }else{
+        $("#lowercase").removeClass("valid");
+    }
+    if(passwordResult.hasUpperCase){
+        $("#uppercase").addClass("valid");
+    }else{
+        $("#uppercase").removeClass("valid");
+    }
+    if(passwordResult.hasNumber){
+        $("#decimal").addClass("valid");
+    }else{
+        $("#decimal").removeClass("valid");
+    }
+
+    if(passwordResult.hasSpecialChar){
+        $("#special").addClass("valid");
+    }else{
+        $("#special").removeClass("valid");
+    }
+
+}
+
+$(document).ready(function () {
+  $("#email").blur(function () {
+    var email = $("#email").val();
+    if (IsEmail(email) == false) {
+      $("#sign-up").attr("disabled", true);
+    } else {
+      $("#sign-up").attr("disabled", false);
+    }
+  });
+  $("#password").keyup(function () {
+    var password = $("#password").val();
+    console.log(password);
+    const passwordResult = checkPassword(password);
+    updatePasswordStrengthUI(passwordResult);
+    if (checkStrength(passwordResult) == false) {
+      $("#sign-up").attr("disabled", true);
+    } else {
+      $("#sign-up").attr("disabled", false);
+    }
+  });
 });
